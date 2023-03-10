cdf5b5393a360804237a8b2fd235bf514425156b
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-03-10 09:51:51 +0630

    Prototype v0.1
    Player can rotate the camera, move forward and backward base on camera position.
    Enemy spawn randomly and follow the
    player

---
+++ Prototype_4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed);
+    }
+}
---
+++ Prototype_4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float randomRange = 9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-randomRange, randomRange);
+        float spawnPosZ = Random.Range(-randomRange, randomRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+}
